fcf9f8261e80d86ef5e3c9c0d9e3890218db2d49
Samuraizan <gainngreen@gmail.com>
2025-03-01 02:06:52 +0530

    Fix Vercel deployment configuration

--- website/next.config.js
+++ website/next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: [],
-  },
-  output: 'standalone',
-  experimental: {
-    serverActions: true
-  }
+  swcMinify: true
 };
 
 module.exports = nextConfig; 
