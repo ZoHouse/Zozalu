cdf028187bf76edb07d40116474a2b633238f7e1
Samuraizan <gainngreen@gmail.com>
2025-03-06 23:35:14 +0530

    refactor: clean up components and consolidate theme functionality

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,23 +1,25 @@
 import Hero from '@/components/Hero';
+import BentoSection from '@/components/BentoSection';
 import TLDR from '@/components/TLDR';
 import EventDetails from '@/components/EventDetails';
 import Infrastructure from '@/components/Infrastructure';
-import Partners from '@/components/Partners';
-import Mentors from '@/components/Mentors';
-import Bounties from '@/components/Bounties';
 import ProofOfVibes from '@/components/ProofOfVibes';
+import Mentors from '@/components/Mentors';
+import Partners from '@/components/Partners';
+import Bounties from '@/components/Bounties';
 
 export default function Home() {
   return (
-    <div className="flex flex-col">
+    <main>
       <Hero />
+      <BentoSection />
       <TLDR />
       <EventDetails />
       <Infrastructure />
-      <Partners />
-      <Mentors />
-      <Bounties />
       <ProofOfVibes />
-    </div>
+      <Mentors />
+      <Partners />
+      <Bounties />
+    </main>
   );
 }
